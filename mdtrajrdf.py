485a6d1a86a88716af90d76356c7f94e16d905d8
Rui Apóstolo <ruiapostolo@gmail.com>
2020-03-09 22:05:49 +0000

    data loading with modified MDAnalysis

--- mdtrajrdf.py
+++ mdtrajrdf.py
@@ -1,46 +1,92 @@
-import tempfile, os
+#  import os
 import argparse
-import numpy as np
-import mdtraj as md
+#  import numpy as np
+import MDAnalysis as mda
+from MDAnalysis.analysis import rdf
 
 try:
     import matplotlib
-
     matplotlib.use('agg')  # no interactive plotting, only save figures
     import pylab
-
     have_matplotlib = True
 except ImportError:
     have_matplotlib = False
 
-
-parser = argparse.ArgumentParser(description='Generate the RDF g(f) of a lammpstrj file')
-parser.add_argument('--trajfile', help='Path to the lammpstrj file',required=True)
-parser.add_argument('--pdbfile', help='Path to the pdb file',required=True)
-parser.add_argument('--pairselect1', help='Atom selection query for first part of pair',required=True)
-parser.add_argument('--pairselect2', help='Atom selection query for first part of pair',required=True)
-parser.add_argument('--outdir', help='Directory to output files into', default='./output')
-parser.add_argument('--outname', help='Name for Output Files',default='rdf')
+parser = argparse.ArgumentParser(
+    description='Generate the RDF g(f) of a lammpstrj file')
+parser.add_argument(
+    '--trajfile',
+    '-t',
+    help='Path to the lammpstrj file',
+    required=True)
+parser.add_argument(
+    '--datafile',
+    '-d',
+    help='Path to the pdb file',
+    required=True)
+parser.add_argument(
+    '--pairselect1',
+    '-a1',
+    help='Atom selection query for first part of pair',
+    required=True)
+parser.add_argument(
+    '--pairselect2',
+    '-a2',
+    help='Atom selection query for first part of pair',
+    required=True)
+parser.add_argument(
+    '--outdir',
+    '-dir',
+    help='Directory to output files into',
+    default='.')
+parser.add_argument(
+    '--outname',
+    '-o',
+    help='Name for Output Files',
+    default='rdf')
 
 args = parser.parse_args()
 
-traj = md.load(args.trajfile,top=args.pdbfile)
+u = mda.Universe(
+    args.datafile,
+    args.trajfile,
+    atom_style="id resid type charge x y z",
+    traj_atom_style="id type x y z vx vy vz fx fy fz",
+    format='LAMMPSDUMP',
+    topology_format='DATA',
+    )
+
 print("Trajectory and Topology Loaded")
-pairs = traj.top.select_pairs(args.pairselect1, args.pairselect2)
-radii, rdf = md.geometry.rdf.compute_rdf(traj, pairs, r_range=(0.0,10.0),bin_width=0.1)
-print("Computed")
+print(u)
+
+sel1 = u.select_atoms(args.pairselect1)
+sel2 = u.select_atoms(args.pairselect2)
+
+ave_rdf = rdf.InterRDF(
+    sel1,
+    sel2,
+    nbins=100,
+    range=(0.0, 10.0),
+    )
+
+ave_rdf.run()
+
+print("RDF run")
 
 outfile = args.outdir+'/'+args.outname+'.dat'
 with open(outfile, 'w') as output:
-    for radius, gofr in zip(radii, rdf):
-        output.write("{radius:8.3f} \t {gofr:8.3f}\n".format(**vars()))
-print ("g(r) data written to {outfile!r}".format(**vars()))
+    for ave_rdf.bins, ave_rdf.rdf in zip(ave_rdf.bins, ave_rdf.rdf):
+        output.write(
+            "{ave_rdf.bins:8.3f} \t {ave_rdf.rdf:8.3f}\n".format(
+                **vars()))
+
+print("g(r) data written to {outfile!r}".format(**vars()))
 
 if have_matplotlib:
     matplotlib.rc('font', size=14)
     matplotlib.rc('figure', figsize=(5, 4))
     pylab.clf()
-    pylab.plot(radii, rdf, linewidth=3)
+    pylab.plot(ave_rdf.bins, ave_rdf.rdf, linewidth=3)
     pylab.xlabel(r"distance $r$ in $\AA$")
     pylab.ylabel(r"radial distribution function $g(r)$")
     pylab.savefig(args.outdir+'/'+args.outname+'.pdf')
